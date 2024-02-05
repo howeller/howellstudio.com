813c18201bc6ed12967a292a7e01b5df3eb2f239
Eric Howell <eric.howell@hogarth-ogilvy.com>
2024-02-05 14:04:26 -0800

    Initial Vite/React setup. Built Header and added SASS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import Header from './components/Header';
+import './scss/style.scss';
+
+function App() {
+
+  return (
+    <Header/>
+  )
+}
+
+export default App;
